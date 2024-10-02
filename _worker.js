b6b12f36103789ab0edfe0ef55e433f451e8a977
a7ad <41451648+a7ad@users.noreply.github.com>
2024-10-03 00:33:17 +0200

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,21 @@
-export default {
-  async fetch(request, env) {
-    let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
-      url.hostname = 'pagesdev.fightlondon.online'
-      let new_request = new Request(url, request);
-      return fetch(new_request);
-    }
-    return env.ASSETS.fetch(request);
-  },};
+addEventListener(
+  "fetch", event => {
+      let url = new URL(event.request.url);
+      url.hostname = "pagesdev.fightlondon.online";
+      url.pathname = "/uploadmedia"
+      url.protocol = "https";
+      let headers = new Headers(event.request.headers);
+      headers.set('Upgrade-Insecure-Requests', '1');
+      headers.set('Host', 'pagesdev.fightlondon.online');
+      headers.set('SNI', 'pagesdev.fightlondon.online');
+
+      let request = new Request(url, {
+        method: event.request.method,
+        headers: headers,
+        body: event.request.body
+      });
+      event.respondWith(
+         fetch(request)
+      )
+  }
+);
