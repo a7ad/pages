f89788331db4375c05df0aca6c1c9d7e8bdd4fa4
a7ad <41451648+a7ad@users.noreply.github.com>
2024-10-03 16:48:56 +0200

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,21 +1,10 @@
-addEventListener(
-  "fetch", event => {
-      let url = new URL(event.request.url);
-      url.hostname = "pagesdev.akbarjojetehran.ir";
-      url.pathname = "/uploadmedia"
-      url.protocol = "https";
-      let headers = new Headers(event.request.headers);
-      headers.set('Upgrade-Insecure-Requests', '1');
-      headers.set('Host', 'pagesdev.akbarjojetehran.ir');
-      headers.set('SNI', 'pagesdev.akbarjojetehran.ir');
-
-      let request = new Request(url, {
-        method: event.request.method,
-        headers: headers,
-        body: event.request.body
-      });
-      event.respondWith(
-         fetch(request)
-      )
-  }
-);
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/')) {
+      url.hostname = 'pagesdev.akbarjojetehran.ir'
+      let new_request = new Request(url, request);
+      return fetch(new_request);
+    }
+    return env.ASSETS.fetch(request);
+  },};
